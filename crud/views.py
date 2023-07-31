7ab8d98ec632d8f4e836813b97d259649f854f16
Sohan Nagarkoti <np03cs4s220012@heraldcollege.edu.np>
2023-07-31 17:21:34 +0545

    Dynamic Post

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from crud.models import Blog, Contactus
@@ -9,21 +10,36 @@ from .forms import Blogfroms
 #     return render(request, "crud/home.html", {"blogs": blog})
 
 def index(request):
-    return render(request, "crud/home.html")
+    blog = Blog.objects.all()
+    return render(request, "crud/home.html", {"blogs": blog})
 
-def post(request):
-    return render(request, "crud/index.html")
+def post(request, id):
+    blog = Blog.objects.get(id=id)
+    return render(request, "crud/post.html", {"blog": blog})
 
 def about(request):
     return render(request, "crud/about.html")
 
-def create(request):
-    form = Blogfroms(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect("home")
-    return render(request, "crud/create.html", {"form": form})
+# def create(request):
+#     form = Blogfroms(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return redirect("home")
+#     return render(request, "crud/create.html", {"form": form})
 
+def create(request):
+    if request.method == "POST":
+        data_title = request.POST.get("title")
+        data_sub_heading = request.POST.get("subheading")
+        data_content = request.POST.get("description")
+        blog = Blog(
+            title=data_title,
+            sub_heading=data_sub_heading,
+            content=data_content,
+        )
+        blog.save()
+        return redirect("crud:home")
+    return render(request, "crud/create.html")
 
 def partData(request, id):
     blog = Blog.objects.get(id=id)
@@ -33,18 +49,18 @@ def partData(request, id):
     return render(request, "crud/index.html",context)
 
 
-def contactus(request):
-    if(request.method == "POST"):
-        dataName = request.POST.get("name")
-        dataEmail = request.POST.get("email")
+# def contactus(request):
+#     if(request.method == "POST"):
+#         dataName = request.POST.get("name")
+#         dataEmail = request.POST.get("email")
         
-        contactus = Contactus(
-            name=dataName,
-            email=dataEmail
-        )
-        contactus.save()
+#         contactus = Contactus(
+#             name=dataName,
+#             email=dataEmail
+#         )
+#         contactus.save()
 
-    return render(request, "crud/contactus.html")
+#     return render(request, "crud/contactus.html")
 
 def deleteBlog(request, id):
     blog = Blog.objects.get(id=id)
